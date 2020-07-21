b77f785d0f9e2537b3907d51224fbdbf78b6f0b3
Toktomambetov Sultan <sultan.toktomambetov.04@bk.ru>
2020-07-21 11:46:33 +0600

    created styles of form

---
+++ src/components/AppFrom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function AppForm() {
+    return (
+        <div className="form">
+            <input className='input' />
+            <button className='addBtn'>
+                add
+            </button>
+        </div>
+    )
+}
