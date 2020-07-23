41913a5dace37e7a3b9647442734c0b63734a45d
sultan <sultan.toktomambetov.04@bk.ru>
2020-07-23 13:05:15 +0600

    added more styles

--- src/components/record.js
+++ src/components/record.js
@@ -1,14 +1,18 @@
 import React from 'react';
 export default function Record(props) {
+    const todoStyles=["todo"]
+    if(props.isDone){
+        todoStyles.push("active");
+    }
     return (
         <div className="record">
-            <h5 className="todo">{props.todo}</h5>
+            <h5 className={todoStyles.join(" ")}>{props.todo}</h5>
             <label className="label">
                 <input type="checkbox" className="checkbox" onChange={props.changeCheck}/>
                 {
                     props.isDone?
-                    (<div className="ans isDone">yes</div>):
-                    (<div className="ans isNotDone">no</div>)
+                    (<div className="ans isDone"><i class="fas fa-check"></i></div>):
+                    (<div className="ans isNotDone"></div>)
                 }
             </label>
             <button className="deleteBtn" onClick={props.deleteRecord}><i className="fas fa-trash-alt"></i></button>
